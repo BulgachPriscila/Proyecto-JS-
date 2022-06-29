60857c4d838bf9795c5b9068aedc785fe782505a
Priscila Bulgach <bulgachpriscila@gmail.com>
2022-06-29 19:43:48 -0300

    Proyecto reformulado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,135 +1,123 @@
 console.log ("Acá inicia el programa.")
 alert ("Bienvenido al Jardín, donde vamos a poder comprar unas plantas!")
 
-function usuario() {
-    
-    let nombre = prompt("Ingrese su nombre: ")
-    let apellido = prompt ("Ingrese su apellido: ")
-    let edad = Number(prompt ("Ingrese su Edad: "))
+let contenedor = ""
+let producto = ``
+let actualizar = document.getElementById ("actualizar").addEventListener("click", actualizarCarrito);
+let plantaCarrito = ""
+let carrito = []
 
-    
-    console.log ("Nombre: " + nombre)
-    console.log ("Apellido: " + apellido)
-    console.log ("Edad: " + edad + " años.")
-}
+const plantas = [
+    {
+        "id" : 1,
+        "nombre" : "Lyrata",
+        "altura" : 20,
+        "tipo" : "Suculenta",
+        "precio" : 200,
+        "img" : "./assets/img/maceta-suculenta.png",
+        "cantidad" : 1
+    },
+    {
+        "id" : 2,
+        "nombre" : "Crassula",
+        "altura" : 15,
+        "tipo" : "Suculenta",
+        "precio" : 150,
+        "img" : "./assets/img/maceta-suculenta.png",
+        "cantidad" : 1
+    },
+    {
+        "id" : 3,
+        "nombre" : "Cactus1",
+        "altura" : 25,
+        "tipo" : "Cactus",
+        "precio" : 200,
+        "img" : "./assets/img/maceta-suculenta.png",
+        "cantidad" : 1
+    },
+    {
+        "id" : 4,
+        "nombre" : "Cactus2",
+        "altura" : 25,
+        "tipo" : "Cactus",
+        "precio" : 180,
+        "img" : "./assets/img/maceta-suculenta.png",
+        "cantidad" : 1
+    }
+]
 
-
-function carrito () {
-    
-}
-
-
-function lyrata () {
-    let planta1Q = Number (prompt("Cuantas Lyrata le gustaría comprar? El precio es $200"))
-    console.log ("Me gustaría comprar " +  planta1Q + " Lyrata")
-    planta1T = planta1Q * 200
-    console.log ("El total de Lyrata sería = $" + planta1T)
-
-}
-function crassula () {
-    let planta2Q = Number (prompt("Cuantas Crassula le gustaría comprar? El precio es $150"))
-    console.log ("Me gustaría comprar " +  planta2Q + " Crassula")
-    planta2T = planta2Q * 150
-    console.log ("El total de Crassula sería = $" + planta2T)
-}
-
-function cactus1 () {
-    let planta3Q = Number (prompt("Cuantos cactus1 le gustaría comprar? El precio es $200"))
-    console.log ("Me gustaría comprar " + planta3Q + " cactus1")
-    planta3T = planta3Q * 200
-    console.log ("El total de cactus1 sería = $" + planta3T)
-}
-function cactus2 () {
-    let planta4Q = Number (prompt("Cuantas cactus2 le gustaría comprar? El precio es $180"))
-    console.log ("Me gustaría comprar " +  planta4Q + " cactus2")
-    planta4T = planta4Q * 180
-    console.log ("El total de cactus2 sería = $" + planta4T)
-}
-
-usuario ()
 class Planta {
-    constructor (id, nombre, altura, tipo, precio) {
+    constructor (id, nombre, altura, tipo, precio, img) {
         this.id = id
         this.nombre = nombre
         this.altura = altura
         this.tipo = tipo 
         this.precio = precio
+        this.img = img
     }
 }
 
-function total () {
-    let totalS = planta1T + planta2T + planta3T + planta4T ;
-    console.log ("El total SIN IVA sería = $" + totalS)    
-
-    let totalI = Number(totalS) * 1.3 ;
-    console.log ("El total CON IVA sería = $" + totalI)
-    alert ("El total sería = $" + totalI)
-}
-
-let planta1T = Number()
-let planta2T = Number()
-let planta3T = Number()
-let planta4T = Number()
-let contenedor = ""
-let producto = ``
-
-const plantas = [
-    new Planta (1 , "lyrata" , 20 , "suculenta" , 200),
-    new Planta (2 , "crassula" , 15 , "suculenta" , 150),
-    new Planta (3 , "cactus1" , 25 , "cactus" , 200),
-    new Planta (4 , "cactus2" , 25 , "cactus" , 180),
-]
-
-function crearShop () {
+function abrirShop () {
     
-    let contenedor = document.getElementById ("container");
+    
+    contenedor = document.getElementById ("container");
     
     plantas.forEach ((plantas) => {
     
         let producto = `
-                        <div class="card col-md">
+                        <div class="card col-md bgtiles">
                             <img src="./assets/img/maceta-suculenta.png" class="card-img-top" alt="Planta">
                             <div class="card-body">
                                 <h5 class="card-title">${plantas.nombre}</h5>
-                                <p class="card-text"><b>${plantas.precio}</b></p>
-                                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number" value="1">
+                                <p class="card-text"><b> $ ${plantas.precio}</b></p>
+                                <button type="submit" class="btn btn-primary" onClick="agregarProducto(${plantas.id})">Agregar</button>
                             </div>
                         </div>
                         `;
                         contenedor.innerHTML += producto
     })
 }
-crearShop ()
+abrirShop ()
 
-let planta1 = prompt ("Le gustaría comprar Lyrata?")
-console.log ("Le gustaría comprar Lyrata? = " + planta1)
+function agregarProducto (id) {
+    let producto = plantas.find (plantas => plantas.id ==id);
 
-if (planta1 == "si") {
-    lyrata ()
+    let plantaCarrito = carrito.find (plantas => plantas.id ==id);
+
+    if (plantaCarrito) {
+        plantaCarrito.cantidad ++;
+    } else {
+        producto.cantidad = 1;
+        carrito.push (producto);
+    }
+    carritoUpdate ()
+
 }
 
-let planta2 = prompt ("Le gustaría comprar Crassula?")
-console.log ("Le gustaría comprar Lyrata? = " + planta2)
-
-if (planta2 == "si") {
-    crassula ()
-} 
-
-
-let planta3 = prompt ("Le gustaría comprar Cactus1?")
-console.log ("Le gustaría comprar Cactus1? = " + planta3)
-
-if (planta3 == "si") {
-    cactus1 ()
+function carritoUpdate () {
+    let carritoCard = document.getElementById ("carrito")
+    html = ""
+    
+    carrito.forEach ((plantas, id) => {
+                
+        html += `
+        <div class="card col-md bgtiles">
+            <img src="./assets/img/maceta-suculenta.png" class="card-img-top" alt="Planta">
+            <div class="card-body">
+                <h5 class="card-title">${plantas.nombre}</h5>
+                <p class="card-text"><b> Precio: $${plantas.precio}</b></p>
+                <p class="card-text"> Cantidad: ${plantas.cantidad}</p>
+                <button type="submit" class="btn btn-danger" onClick="eliminarProducto(${plantas.id})">Eliminar</button>
+            </div>
+        </div>
+        `
+    })
+    carritoCard.innerHTML = html 
+}
+/* function eliminarProducto(){
+} */
+function actualizarCarrito () { 
+    alert ("Carrito actualizado")
+    carritoUpdate()
 }
 
-
-let planta4 = prompt ("Le gustaría comprar Cactus2?")
-console.log ("Le gustaría comprar Lyrata? = " + planta4)
-
-if (planta4 == "si") {
-    cactus2()
-} 
-
-carrito ()
-total ()
